fc58ab5ccb31feaa78afa82093346e5635888a40
hisanori <hisary@gmail.com>
2013-12-30 23:31:02 +0900

    setupスクリプトを修正

--- setup.sh
+++ setup.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+SCRIPT_FILE=${0##*/}
 DOTFILE_DIR=$(cd $(dirname $0); pwd)
+
+
+# --- functions
+
+log() {
+	echo $@
+} >&2
+
 
 # --- copy dotfile
 
-FILE_LIST=`ls $DOTFILE_DIR | grep -v setup.sh`
+FILE_LIST=$(ls $DOTFILE_DIR | grep -v $SCRIPT_FILE)
 
 for i in $FILE_LIST; do
 
@@ -12,10 +21,10 @@ for i in $FILE_LIST; do
 	FILE_DST=${HOME}/.${i}
 
 	if [ -L $FILE_DST ]; then
-		rm $FILE_DST && echo "${FILE_DST} is symbolic link, remove it."
+		rm $FILE_DST && log "${FILE_DST} is symbolic link, remove it."
 
 	elif [ -e $FILE_DST ]; then
-		mv $FILE_DST $FILE_DST.org && echo "${FILE_DST} already exists, rename it."
+		mv $FILE_DST $FILE_DST.org && log "${FILE_DST} already exists, rename it."
 	fi
 	
 	ln -s $FILE_SRC $FILE_DST
@@ -32,6 +41,6 @@ fi
 if [ ! -d ${VUNDLE_DIR}/vundle ]; then
 	git clone https://github.com/gmarik/vundle ${VUNDLE_DIR}/vundle
 else
-	echo "Vundle is already installed"
+	log "Vundle have been already installed."
 fi
 
