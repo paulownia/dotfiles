f74e8956091155c09553bb5b8dd5b25dc677ce38
hisanori <hisary@gmail.com>
2023-03-27 18:59:57 +0900

    nvim: replace kommentary plugin with nvim_comment plugin

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,7 +1,10 @@
 vim.cmd('source ~/.vimrc')
 
+--[[
 require('kommentary.config').use_extended_mappings()
 vim.api.nvim_set_keymap("n", "gcc", "<Plug>kommentary_line_default", {})
 vim.api.nvim_set_keymap("n", "gc", "<Plug>kommentary_motion_default", {})
 vim.api.nvim_set_keymap("v", "gc", "<Plug>kommentary_visual_default<C-c>", {})
+]]--
 
+require('nvim_comment').setup()
