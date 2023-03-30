05b8b48fc2ab233f0f5b3dcb50bff0af270cf3f9
hisanori <hisary@gmail.com>
2023-03-30 10:37:02 +0900

    nvim: add nightfox plugin (colorscheme)

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,8 +1,28 @@
 vim.cmd('source ~/.vimrc')
 
+vim.opt.termguicolors = true
+vim.opt.wildoptions = 'pum'
+vim.opt.pumblend = 10
+
 require('nvim_comment').setup()
 
 require("nvim-tree").setup()
 
 vim.api.nvim_set_keymap("n", "T", ":NvimTreeToggle<CR>", { noremap = true })
+
+require('nightfox').setup({
+  options = {
+    transparent = true
+  },
+  groups = {
+    all = {
+      Visual = { bg = "#60626A" },
+      Search = { bg = "#606032" },
+      VertSplit = { bg = "#505050", fg = "#505050" }
+    }
+  },
+});
+
+vim.cmd.colorscheme "carbonfox"
+
 
