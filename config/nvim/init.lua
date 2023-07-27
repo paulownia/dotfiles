372fceabffe8563aa4b2eee437e4a10aaee53316
hisanori <hisary@gmail.com>
2023-07-27 18:53:14 +0900

    vim: customize diff colors

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -10,18 +10,31 @@ require("nvim-tree").setup()
 
 vim.api.nvim_set_keymap("n", "T", ":NvimTreeToggle<CR>", { noremap = true })
 
+
+local C = require("nightfox.lib.color")
+
 require('nightfox').setup({
   options = {
     transparent = true
   },
   groups = {
-    all = {
+    carbonfox = {
       Pmenu = { bg = "#204148" },     -- 補完メニューなど
       Visual = { bg = "#60626A" },    -- v-modeでの選択範囲
       Search = { bg = "#606032" },    -- 検索結果
       VertSplit = { bg = "#505050", fg = "#505050" },    -- ウィンドウ分割時の縦線 `|`
     }
   },
+  specs = {
+    carbonfox = {
+      diff = {
+        add = C("#161616"):blend(C("#25BE6A"), 0.25):to_css(),
+        delete = C("#161616"):blend(C("#EE5396"), 0.2):to_css(),
+        change = C("#161616"):blend(C("#78A9FF"), 0.3):to_css(),
+        text = "#5C03FE",
+      }
+    }
+  }
 });
 
 vim.cmd.colorscheme "carbonfox"
